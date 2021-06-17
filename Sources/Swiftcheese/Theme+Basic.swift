d73fdbd661aba2e0d8af403bd577cf07ce93a075
Chuck Underwood <chillieguy@live.com>
2021-06-17 16:45:57 -0700

    Add theme file

--- Sources/Swiftcheese/Theme+Basic.swift
+++ Sources/Swiftcheese/Theme+Basic.swift
@@ -1,8 +1,245 @@
 //
-//  File.swift
+//  Theme+Basic.swift
 //  
 //
 //  Created by Chuck Underwood on 6/17/21.
 //
 
 import Foundation
+import Publish
+import Plot
+
+extension Theme where Site == Swiftcheese {
+    /// My customized "Basic" theme.
+    static var basic: Self {
+        Theme(
+            htmlFactory: BasicHTMLFactory(),
+            resourcePaths: ["Resources/BasicTheme/styles.css"]
+        )
+    }
+}
+
+private struct BasicHTMLFactory: HTMLFactory {
+    func makeIndexHTML(for index: Index,
+                       context: PublishingContext<Swiftcheese>) throws -> HTML {
+        HTML(
+            .lang(context.site.language),
+            .head(for: index, on: context.site),
+            .body {
+                SiteHeader(context: context, selectedSelectionID: nil)
+                Wrapper {
+                    H1(index.title)
+                    Paragraph(context.site.description)
+                        .class("description")
+                    H2("Latest content")
+                    ItemList(
+                        items: context.allItems(
+                            sortedBy: \.date,
+                            order: .descending
+                        ),
+                        site: context.site
+                    )
+                }
+                SiteFooter()
+            }
+        )
+    }
+
+    func makeSectionHTML(for section: Section<Swiftcheese>,
+                         context: PublishingContext<Swiftcheese>) throws -> HTML {
+        HTML(
+            .lang(context.site.language),
+            .head(for: section, on: context.site),
+            .body {
+                SiteHeader(context: context, selectedSelectionID: section.id)
+                Wrapper {
+                    H1(section.title)
+                    ItemList(items: section.items, site: context.site)
+                }
+                SiteFooter()
+            }
+        )
+    }
+
+    func makeItemHTML(for item: Item<Swiftcheese>,
+                      context: PublishingContext<Swiftcheese>) throws -> HTML {
+        HTML(
+            .lang(context.site.language),
+            .head(for: item, on: context.site),
+            .body(
+                .class("item-page"),
+                .components {
+                    SiteHeader(context: context, selectedSelectionID: item.sectionID)
+                    Wrapper {
+                        Article {
+                            Div(item.content.body).class("content")
+                            Span("Tagged with: ")
+                            ItemTagList(item: item, site: context.site)
+                        }
+                    }
+                    SiteFooter()
+                }
+            )
+        )
+    }
+
+    func makePageHTML(for page: Page,
+                      context: PublishingContext<Swiftcheese>) throws -> HTML {
+        HTML(
+            .lang(context.site.language),
+            .head(for: page, on: context.site),
+            .body {
+                SiteHeader(context: context, selectedSelectionID: nil)
+                Wrapper(page.body)
+                SiteFooter()
+            }
+        )
+    }
+
+    func makeTagListHTML(for page: TagListPage,
+                         context: PublishingContext<Swiftcheese>) throws -> HTML? {
+        HTML(
+            .lang(context.site.language),
+            .head(for: page, on: context.site),
+            .body {
+                SiteHeader(context: context, selectedSelectionID: nil)
+                Wrapper {
+                    H1("Browse all tags")
+                    List(page.tags.sorted()) { tag in
+                        ListItem {
+                            Link(tag.string,
+                                 url: context.site.path(for: tag).absoluteString
+                            )
+                        }
+                        .class("tag")
+                    }
+                    .class("all-tags")
+                }
+                SiteFooter()
+            }
+        )
+    }
+
+    func makeTagDetailsHTML(for page: TagDetailsPage,
+                            context: PublishingContext<Swiftcheese>) throws -> HTML? {
+        HTML(
+            .lang(context.site.language),
+            .head(for: page, on: context.site),
+            .body {
+                SiteHeader(context: context, selectedSelectionID: nil)
+                Wrapper {
+                    H1 {
+                        Text("Tagged with ")
+                        Span(page.tag.string).class("tag")
+                    }
+
+                    Link("Browse all tags",
+                        url: context.site.tagListPath.absoluteString
+                    )
+                    .class("browse-all")
+
+                    ItemList(
+                        items: context.items(
+                            taggedWith: page.tag,
+                            sortedBy: \.date,
+                            order: .descending
+                        ),
+                        site: context.site
+                    )
+                }
+                SiteFooter()
+            }
+        )
+    }
+}
+
+private struct Wrapper: ComponentContainer {
+    @ComponentBuilder var content: ContentProvider
+
+    var body: Component {
+        Div(content: content).class("wrapper")
+    }
+}
+
+private struct SiteHeader<Swiftcheese: Website>: Component {
+    var context: PublishingContext<Swiftcheese>
+    var selectedSelectionID: Swiftcheese.SectionID?
+
+    var body: Component {
+        Header {
+            Wrapper {
+                Link(context.site.name, url: "/")
+                    .class("site-name")
+
+                if Swiftcheese.SectionID.allCases.count > 1 {
+                    navigation
+                }
+            }
+        }
+    }
+
+    private var navigation: Component {
+        Navigation {
+            List(Swiftcheese.SectionID.allCases) { sectionID in
+                let section = context.sections[sectionID]
+
+                return Link(section.title,
+                    url: section.path.absoluteString
+                )
+                .class(sectionID == selectedSelectionID ? "selected" : "")
+            }
+        }
+    }
+}
+
+private struct ItemList<Swiftcheese: Website>: Component {
+    var items: [Item<Swiftcheese>]
+    var site: Swiftcheese
+
+    var body: Component {
+        List(items) { item in
+            Article {
+                H1(Link(item.title, url: item.path.absoluteString))
+                H2(Text("Published on: \(item.date.asText)"))
+                ItemTagList(item: item, site: site)
+                Paragraph(item.description)
+            }
+        }
+        .class("item-list")
+    }
+}
+
+private struct ItemTagList<Swiftcheese: Website>: Component {
+    var item: Item<Swiftcheese>
+    var site: Swiftcheese
+
+    var body: Component {
+        List(item.tags) { tag in
+            Link(tag.string, url: site.path(for: tag).absoluteString)
+        }
+        .class("tag-list")
+    }
+}
+
+private struct SiteFooter: Component {
+    var body: Component {
+        Footer {
+            Paragraph {
+                Text("Generated using ")
+                Link("Publish", url: "https://github.com/johnsundell/publish")
+            }
+            Paragraph {
+                Link("RSS feed", url: "/feed.rss")
+            }
+        }
+    }
+}
+
+extension Date {
+    var asText: String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd MMMM, yyyy"
+        let dateString = formatter.string(from: self)
+        return dateString
+    }
+}
